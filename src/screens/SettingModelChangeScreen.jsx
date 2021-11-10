e074949ebbb7b759241ec3a947a540173c3e4cae
KagYeah <69356547+KagYeah@users.noreply.github.com>
2021-11-10 15:56:29 +0900

    Modify SettingModelChangeScreen

--- src/screens/SettingModelChangeScreen.jsx
+++ src/screens/SettingModelChangeScreen.jsx
@@ -1,48 +1,42 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import {
-  ImageBackground, ScrollView, StyleSheet, Text, TextInput, View,
+  ImageBackground, ScrollView, StyleSheet, Text, View,
 } from 'react-native';
 
 import { BackgroundImageContext, ThemeContext } from '../contexts';
-import Button from '../components/Button';
+import ListItem from '../components/ListItem';
 import { appStyles } from '../style';
 
-export default function SettingModelChangeScreen(props) {
+export default function SettingModelChangeScreen() {
   const { backgroundImage } = useContext(BackgroundImageContext);
   const { theme } = useContext(ThemeContext);
-  const { navigation } = props;
-  const [password, setPassword] = useState('');
 
-  const message = `あなたのIDは「abc123」です。以下でパスワードを設定し、ID・パスワードを大事に保管してください。
+  const message = `現在、「test@example.com」でログイン中です。
 
-設定が完了して24時間以内に、機種変更後の端末にデータを移行してください。データの移行は、本アプリ初回起動時に「機種変更」ボタンを押してから行ってください。`;
+機種変更する際は、「データをアップロード」でバックアップを取り、新しい端末で、「データをダウンロード」してください。機種変更にはログインする必要があります。
+
+データをダウンロードすると、前のデータは消てしまいます。ご注意ください。`;
 
   return (
     <View style={styles(theme).container}>
       <ImageBackground source={{ uri: backgroundImage }} resizeMode="cover" style={{ flex: 1 }}>
 
-        <ScrollView style={styles(theme).scroll}>
-          <Text style={styles(theme).description}>{message}</Text>
-
-          <TextInput
-            onChangeText={(text) => setPassword(text)}
-            placeholder="パスワード"
-            style={styles(theme).input}
-            value={password}
+        <ScrollView>
+          <ListItem
+            title="データをアップロード"
+            onPress={() => {}}
+            style={styles(theme).listItem}
           />
 
-          <Button
-            label="設定"
-            onPress={() => {
-              navigation.goBack();
-            }}
-            color={appStyles(theme).buttonMedium.color}
-            style={styles(theme).submit}
-            height={appStyles(theme).buttonMedium.height}
-            width={appStyles(theme).buttonMedium.width}
-            linearGradient
-            options={{ colors: appStyles(theme).buttonMedium.gradientColors }}
+          <ListItem
+            title="データをダウンロード"
+            onPress={() => {}}
+            style={styles(theme).listItem}
           />
+
+          <View style={styles(theme).description}>
+            <Text style={styles(theme).text}>{message}</Text>
+          </View>
         </ScrollView>
       </ImageBackground>
     </View>
@@ -54,24 +48,15 @@ const styles = (theme) => StyleSheet.create({
     backgroundColor: appStyles(theme).app.backgroundColor,
     flex: 1,
   },
-  scroll: {
-    paddingHorizontal: appStyles(theme).app.paddingHorizontal,
-    paddingVertical: appStyles(theme).app.paddingVertical,
+  listItem: {
+    fontSize: 18,
+    height: 48,
   },
   description: {
+    padding: 24,
+  },
+  text: {
     fontSize: appStyles(theme).body.fontSize,
     lineHeight: appStyles(theme).body.lineHeight,
-  },
-  input: {
-    borderColor: appStyles(theme).idPasswordInput.borderColor,
-    borderWidth: appStyles(theme).idPasswordInput.borderWidth,
-    fontSize: appStyles(theme).idPasswordInput.fontSize,
-    height: appStyles(theme).idPasswordInput.height,
-    padding: appStyles(theme).idPasswordInput.padding,
-    marginTop: appStyles(theme).idPasswordInput.margin,
-  },
-  submit: {
-    alignSelf: 'center',
-    marginTop: appStyles(theme).buttonMedium.margin,
   },
 });
