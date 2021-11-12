c84d91f68ce77b82ea7016d7532e606a019b52c0
KagYeah <69356547+KagYeah@users.noreply.github.com>
2021-11-12 22:23:59 +0900

    Add env.js to .gitignore

--- env.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCJo56OZjQwdVJJOULTB0EFRQVhbEjaNI0',
-  authDomain: 'mynotes-15018.firebaseapp.com',
-  projectId: 'mynotes-15018',
-  storageBucket: 'mynotes-15018.appspot.com',
-  messagingSenderId: '739267415739',
-  appId: '1:739267415739:web:c7cf105a953e807249639e',
-};
